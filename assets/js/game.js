5f98b372c5d692ec1d95e03e70a1f1bd05c97163
Nicholas Delpino <NBDelpino4@gmail.com>
2017-02-04 01:13:31 -0500

    fixed JS problem

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,39 +1,39 @@
-var numWins = 0;
-var numLooses = 0;
-var guessLeft = 10;
-var guessUsed = [];
+var numUserWins = 0;
+var numUserLosses = 0;
+var numGuessesRemaining = 10;
+var guessedAlready = [];
 var myletter = ["D", "E", "L", "P", "I", "N", "O"];
 var randltr = myletter[Math.floor(Math.random() * myletter.length)];
 window.onkeypress = function(event) {
     var usersGuess = String.fromCharCode(event.keyCode).toUpperCase();
-    guessUsed.push(usersGuess);
+    guessedAlready.push(usersGuess);
     console.log(randltr);
     if (usersGuess == randltr) {
-        numWins++;
+        numUserWins++;
         alert("you won the correct letter was:" + randltr);
-        document.getElementById("numUserWins").innerHTML = "Win: " + numWins;
+        document.getElementById("numUserWins").innerHTML = "Win: " + numUserWins;
         restart();
     } else {
-        if (guessLeft == 0) {
+        if (numGuessesRemaining == 0) {
             alert("You lost");
-            numLooses++;
-            document.getElementById("numUserLosses").innerHTML = "Loss : " + numLooses;
+            numUserLosses++;
+            document.getElementById("numUserLosses").innerHTML = "Loss : " + numUserLosses;
             restart();
         } else {
             alert("Opps try again");
             document.getElementById("currentGuess").innerHTML = "Current Guess : " + usersGuess;
-            guessLeft--;
-            document.getElementById("numGuessesRemaining").innerHTML = "Guesses Remaining : " + guessLeft;
-            document.getElementById("guessedAlready").innerHTML = "Guessed Already : " + guessUsed.join(" ");
+            numGuessesRemaining--;
+            document.getElementById("numGuessesRemaining").innerHTML = "Guesses Remaining : " + numGuessesRemaining;
+            document.getElementById("guessedAlready").innerHTML = "Guessed Already : " + guessedAlready.join(" ");
         }
     }
 }
 
-// function restart() {
-//     document.getElementById("guessedAlready").innerHTML = "Guesses so far : ";
-//     document.getElementById("currentGuess").innerHTML = "You guess: ";
-//     document.getElementById("numGuessesRemaining").innerHTML = "Guesses left: 10 ";
-//     randltr = myletter[Math.floor(Math.random() * myletter.length)];
-//     guessUsed = [];
-//     guessLeft = 10;
-// }
+function restart() {
+    // document.getElementById("guessedAlready").innerHTML = "Guesses so far : ";
+    // document.getElementById("currentGuess").innerHTML = "You guess: ";
+    // document.getElementById("numGuessesRemaining").innerHTML = "Guesses left: 10 ";
+    randltr = myletter[Math.floor(Math.random() * myletter.length)];
+    guessedAlready = [];
+    numGuessesRemaining = 10;
+}
